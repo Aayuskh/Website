529d63bbe302ee931e47541d9d8191e06407c143
Wang, Zhenhua <wangzhenhua.mail@gmail.com>
2013-05-05 10:08:33 -0400

    changes

---
+++ www/js/framework.js
@@ -0,0 +1,28 @@
+var selected="home";
+function gotoMap(obj){
+    
+    $("#"+selected+"Tab img").attr("src","img/"+selected+".png");
+    $(obj).find("img").attr("src","img/map_w.png");
+    
+    selected="map";
+}
+function gotoHome(obj){
+    
+    $("#"+selected+"Tab img").attr("src","img/"+selected+".png");
+    $(obj).find("img").attr("src","img/home_w.png");
+    
+    selected="home";
+}
+function gotoTicket(obj){
+
+    $("#"+selected+"Tab img").attr("src","img/"+selected+".png");
+    $(obj).find("img").attr("src","img/ticket_w.png");
+    
+    selected="ticket";
+}
+function gotoSetting(obj){
+    $("#"+selected+"Tab img").attr("src","img/"+selected+".png");
+    $(obj).find("img").attr("src","img/setting_w.png");
+    
+    selected="setting";
+}
