3c2fbc33143dc5bf71aa71d48067131b2c0aa726
anotherdaylia <zheng.lia.qu@gmail.com>
2013-05-06 20:32:28 -0400

    update map

--- www/js/map.js
+++ www/js/map.js
@@ -1,10 +1,15 @@
-function initialize()
-{
-var mapProp = {
-  center:new google.maps.LatLng(51.508742,-0.120850),
-  zoom:5,
-  mapTypeId:google.maps.MapTypeId.ROADMAP
-  };
-var map=new google.maps.Map(document.getElementById("googleMap")
-  ,mapProp);
+
+
+function initialize(){
+
+	var mapProp = {
+		center: new google.maps.LatLng(40.4441236,-79.94534760),
+		zoom:12,
+		disableDefaultUI: true,
+		mapTypeId:google.maps.MapTypeId.ROADMAP
+	};
+
+	var map = new google.maps.Map(document.getElementById("googleMap")
+	  ,mapProp);
+
 }
