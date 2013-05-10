3ee817ebafc52992fdd0e6e5ba4a5d53374ec9d2
Wang, Zhenhua <wangzhenhua.mail@gmail.com>
2013-05-09 21:02:10 -0400

    slow

--- www/js/framework.js
+++ www/js/framework.js
@@ -14,15 +14,21 @@ function gotoHome(obj){
     selected="home";
 }
 function gotoTicket(obj){
-
+go("./setting.html");
     $("#"+selected+"Tab img").attr("src","img/"+selected+".png");
     $(obj).find("img").attr("src","img/ticket_w.png");
     
     selected="ticket";
+    
 }
 function gotoSetting(obj){
     $("#"+selected+"Tab img").attr("src","img/"+selected+".png");
     $(obj).find("img").attr("src","img/setting_w.png");
     
     selected="setting";
+}
+function go(url){
+    //$("#mainContainer").load(url);
+    
+    $.mobile.changePage(url);
 }
