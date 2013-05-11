3d65d8573b2e82837d611ebe730f2dc249a50b6a
Wang, Zhenhua <wangzhenhua.mail@gmail.com>
2013-05-10 20:14:17 -0400

    sdfasd

--- www/js/framework.js
+++ www/js/framework.js
@@ -7,14 +7,14 @@ function gotoMap(obj){
     selected="map";
 }
 function gotoHome(obj){
-    
+    go("./index.html");
     $("#"+selected+"Tab img").attr("src","img/"+selected+".png");
     $(obj).find("img").attr("src","img/home_w.png");
     
     selected="home";
 }
 function gotoTicket(obj){
-go("./setting.html");
+go("./ticket.html");
     $("#"+selected+"Tab img").attr("src","img/"+selected+".png");
     $(obj).find("img").attr("src","img/ticket_w.png");
     
@@ -32,3 +32,25 @@ function go(url){
     
     $.mobile.changePage(url);
 }
+function register(){
+    var ref = window.open("https://www.portauthority.org/paac/CreateAccount/tabid/212/Default.aspx?returnurl=http%3a%2f%2fwww.portauthority.org%2fpaac%2fFaresPasses%2fFares.aspx", '_system', 'location=false');
+}
+function buy(){
+
+}
+function takepic(){
+    alert("Please take a picture of yourself. Picture cannot be changed after taken.");
+    navigator.camera.getPicture(onSuccess, onFail, { quality: 50,
+                                destinationType: Camera.DestinationType.FILE_URI });
+
+}
+function onSuccess(imageURI) {
+    window.localStorage.setItem("studentPass", "true");
+    window.localStorage.setItem("studentPass.img", imageURI);
+    $('#studentRegistry').popup("close");
+}
+
+function onFail(message) {
+    alert('Failed because: ' + message);
+}
+
